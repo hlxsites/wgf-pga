c65fa54fb8de127f4b342fff0a1e7937f1447cd2
David Nuescheler <david@nuescheler.ch>
2022-08-01 08:58:48 -0700

    chore: add container logic

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -2,4 +2,7 @@ export default function decorate(block) {
   block.querySelectorAll('img').forEach((img) => {
     img.closest('div').classList.add('cards-contains-image');
   });
+  if (block.classList.contains('horizontal')) {
+    block.closest('.section').classList.add('cards-horizontal-container');
+  }
 }
