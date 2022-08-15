28d7f00522c7cb473f2317284843fe3c82ad7acb
David Nuescheler <david@nuescheler.ch>
2022-08-15 15:25:19 -0600

    chore: optanon

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,12 +1,15 @@
 // eslint-disable-next-line import/no-cycle
 import { sampleRUM } from './scripts.js';
 
-function loadScript(url, callback, type) {
+function loadScript(url, callback, attr) {
   const $head = document.querySelector('head');
   const $script = document.createElement('script');
   $script.src = url;
-  if (type) {
-    $script.setAttribute('type', type);
+  if (attr) {
+    const keys = Object.keys(attr);
+    keys.forEach((k) => {
+      $script.setAttribute(k, attr[k]);
+    });
   }
   $head.append($script);
   $script.onload = callback;
@@ -50,4 +53,9 @@ window.pgatour.Omniture = {
 };
 window.pgatour.docWrite = document.write.bind(document);
 
-loadScript('https://assets.adobedtm.com/d17bac9530d5/90b3c70cfef1/launch-1ca88359b76c.min.js');
+loadScript('https://cdn.cookielaw.org/scripttemplates/otSDKStub.js', () => {
+  loadScript('https://assets.adobedtm.com/d17bac9530d5/90b3c70cfef1/launch-1ca88359b76c.min.js');
+}, {
+  'data-domain-script': '262c6c79-a114-41f0-9c07-52cb1fb7390c',
+})
+
