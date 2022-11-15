a8806212e2d3e099803b1cdeb10be6b81dd7350d
Katie Sexton <ksexton@adobe.com>
2022-11-15 13:18:42 -0700

    fix: update cookie choices link

--- blocks/footer/footer.js
+++ blocks/footer/footer.js
@@ -1,10 +1,21 @@
 import { readBlockConfig, decorateIcons, decorateLinkedPictures } from '../../scripts/scripts.js';
 
+function setupCookieChoices(section) {
+  if (section) {
+    const cookieLink = section.querySelector('a[href*="onetrust-link"]');
+    if (cookieLink) {
+      cookieLink.removeAttribute('href');
+      cookieLink.className = 'ot-sdk-show-settings';
+      cookieLink.id = 'ot-sdk-btn';
+      cookieLink.parentNode.className = 'onetrust-link';
+    }
+  }
+}
+
 /**
  * loads and decorates the footer
  * @param {Element} block The header block element
  */
-
 export default async function decorate(block) {
   const cfg = readBlockConfig(block);
   block.textContent = '';
@@ -21,4 +32,5 @@ export default async function decorate(block) {
   styles.forEach((style, i) => {
     if (footer.children[i]) footer.children[i].classList.add(`footer-${style}`);
   });
+  setupCookieChoices(footer.querySelector('.footer-legal'));
 }
